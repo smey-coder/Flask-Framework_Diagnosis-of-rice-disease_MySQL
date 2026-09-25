9fa07a522bf7ba7d5af2930b01b79594aac8fb21
Your Name <sanreaksmey01@gmail.com>
2026-09-25 22:38:22 +0700

    smey

--- app/routes/user_route/user_route.py
+++ app/routes/user_route/user_route.py
@@ -3220,7 +3220,7 @@ def add_harvest():
         if selected_field:
             crops = (
                 FieldCropTable.query.options(db.joinedload(FieldCropTable.rice_variety))
-                .filter_by(field_id=selected_field, status="Active")
+                .filter_by(field_id=selected_field, status="Harvested")
                 .all()
             )
             form.field_crop_id.choices = [
